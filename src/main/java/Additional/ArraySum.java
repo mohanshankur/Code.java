058c1d599a6671f3a49b3db55056faf843a903e1
Mohan Shankur <mohanshankur@Mohans-MacBook-Air.local>
2024-02-01 14:48:15 -0600

    Additional

---
+++ src/main/java/Additional/ArraySum.java
@@ -0,0 +1,22 @@
+package Additional;
+
+public class ArraySum {
+
+        public static void main(String[] args) {
+            int[] array = {1, 2, 3, 4, 5};
+
+            int sum = calculateSum(array);
+
+            System.out.println("Sum of all items in the array: " + sum);
+        }
+
+        public static int calculateSum(int[] array) {
+            int sum = 0;
+            for (int num : array) {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+
+
---
+++ src/main/java/Additional/Swapping.java
@@ -0,0 +1,13 @@
+package Additional;
+
+public class Swapping {
+
+
+        public static void swapWithoutTemp(int a, int b) {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            System.out.println("After swapping: a = " + a + ", b = " + b );
+        }
+    }
+
