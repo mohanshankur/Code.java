6d715b8b87a031f67f2a1b1cede9367c24254e6a
Mohan Shankur <mohanshankur@Mohans-MacBook-Air.local>
2024-02-02 15:51:43 -0600

    Additional

---
+++ src/main/java/Additional/Rotation.java
@@ -0,0 +1,18 @@
+package Additional;
+
+public class Rotation {
+
+
+    public static boolean Rotations(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        String concatenated = str1 + str1;
+
+        return concatenated.contains(str2);
+    }
+}
+
+
---
+++ src/test/java/AdditionalTest/RotationTest.java
@@ -0,0 +1,33 @@
+package AdditionalTest;
+
+import Additional.Rotation;
+import org.junit.Assert;
+import Additional.Rotation;
+import org.junit.Assert;
+import org.junit.Assert;
+import org.junit.jupiter.api  .Test;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+public class RotationTest {
+    
+        @org.junit.Test
+        public void TestRotation()
+
+        {
+            Rotation r = new Rotation();
+            Boolean out =r.Rotations("JavaJ2eeStrutsHibernate","StrutsHibernateJavaJ2ee");
+            Assert.assertTrue(out);
+        }
+
+        @org.junit.Test
+        public void TestRotation1()
+
+        {
+            Rotation r1 = new Rotation();
+            Boolean out =r1.Rotations("Mohan","Sai");
+            Assert.assertFalse(out);
+        }
+    }
+
+
