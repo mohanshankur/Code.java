427be8b27ea3ca0388755f09a923760a644f46cf
Mohan Shankur <mohanshankur@Mohans-Air.lan>
2024-02-11 22:45:07 -0600

    exceptionHandling

---
+++ src/main/java/exceptionHandling/InvalidWithdrawalException.java
@@ -0,0 +1,9 @@
+package exceptionHandling;
+
+
+// InvalidWithdrawalException.java
+public class InvalidWithdrawalException extends Exception {
+    public InvalidWithdrawalException(String message) {
+        super(message);
+    }
+}
